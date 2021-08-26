4357dd0c1ab0509e04ee596db836b77db5e7a00a
Muhammad Irfan <irfanmuh75@gmail.com>
2021-08-26 22:52:47 +0700

    added post text

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default ({ data }) => {
       <Seo title="Home" />
       <div>
         <h1>Irfan's Thoughts</h1>
-        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        <h4>{data.allMarkdownRemark.totalCount} Post</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
             <BlogLink to={node.fields.slug}>
